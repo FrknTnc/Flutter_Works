a289c3b7bd4486a5b7da7749e4ac1c74201dfc3c
Furkan Tunç <furkanali.2001@gmail.com>
2023-02-20 13:34:44 +0300

    Update dört_button_learn.dart

--- lib/101/dört_button_learn.dart
+++ lib/101/dört_button_learn.dart
@@ -10,31 +10,45 @@ class ButtonLearnWidget extends StatelessWidget {
       body: Column(
         children: [
           TextButton(
-            onPressed: () {},
-            child: Text('aaaaa', style: Theme.of(context).textTheme.subtitle1),
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith((states) {
-              if (states.contains(MaterialState.pressed)) {
-                return Colors.green;
-              }
-              return Colors.white;
-            })),
-          ),
+              onPressed: () {},
+              child:
+                  Text('aaaaa', style: Theme.of(context).textTheme.subtitle1),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith((states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return Colors.green;
+                }
+                return Colors.white;
+              }))),
           ElevatedButton(onPressed: () {}, child: Text('dataa')),
           IconButton(
               onPressed: () {}, icon: const Icon(Icons.add_photo_alternate)),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              //servise istek
+              //sayfa rengi
+            },
             child: const Icon(Icons.add_alarm_sharp),
           ),
           OutlinedButton(
               style: OutlinedButton.styleFrom(
-                  backgroundColor: Colors.red, shape: CircleBorder()),
+                backgroundColor: Colors.red,
+                padding: EdgeInsets.all(10),
+                //shape: CircleBorder()
+              ),
               onPressed: () {},
-              child: const Text('data')),
+              child: const SizedBox(width: 200, child: Text('data'))),
+          OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.abc),
+              label: const Text('DATA')),
           InkWell(
             onTap: () {},
             child: const Text('custom'),
+          ),
+          Container(
+            height: 200,
+            color: Colors.white,
           )
         ],
       ),
