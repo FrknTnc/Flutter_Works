a4b99edb363fa296b5aed6984bf7411bd23c73ec
Furkan Tunç <furkanali.2001@gmail.com>
2023-02-20 18:58:10 +0300

    Create yedi_color_learn.dart

---
+++ lib/101/yedi_color_learn.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorLearn extends StatelessWidget {
+  const ColorLearn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: ,
+        child: const Text("data"),
+      ),
+    );
+  }
+}
